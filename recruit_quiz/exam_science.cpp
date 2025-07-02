c60ecc5ed31f7044c93ad28b7601a6922095c472
segawak <segawakeigo@gmail.com>
2025-07-02 14:50:09 +0900

    水中の浮力の問題を追加

--- recruit_quiz/exam_science.cpp
+++ recruit_quiz/exam_science.cpp
@@ -100,6 +100,21 @@ QuestionLIst CreatePhysicsExam()
 			to_string(h) + "cmの円柱を完全に水中に沈めた。\n" + "このとき、この円柱に働く浮力はxニュートンである。\n" +
 			"Xの値を小数点以下第２位を四捨五入して求めよ。",
 			answer });
+
+		int x = uniform_int_distribution<>(20, 50)(rd);//重さ(空中)
+		int y = uniform_int_distribution<>(x / 2, x - 1)(rd);//重さ(水中)
+		int z = x - y + 5;
+		answer = to_string(z / 100);
+		z /= 10;
+		if (z % 10) {
+			answer += '.';
+			answer += '0' + z % 10;
+		}
+		questions.push_back({
+			"質量100gの物体に働く重力を1Nとする。\nある物体の重さをばねはかりではかると、" +
+			to_string(x) + "gを示した\n" + "子の物体を完全に水に入れたところ、ばねはかりは" +
+			to_string(y) + "gを示した\nこのとき、物体に働く浮力はXニュートンである。\n" +
+			"Xの値を小数点以下第２位を四捨五入して求めよ",answer });
 	}//浮力
 
 	return questions;
