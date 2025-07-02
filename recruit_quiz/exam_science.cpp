dcbbea962c01934fbd75f7f4d592dcb2ce81b2cd
segawak <segawakeigo@gmail.com>
2025-07-02 14:57:51 +0900

    水に浮く物体の浮力の問題を追加

--- recruit_quiz/exam_science.cpp
+++ recruit_quiz/exam_science.cpp
@@ -115,6 +115,20 @@ QuestionLIst CreatePhysicsExam()
 			to_string(x) + "gを示した\n" + "子の物体を完全に水に入れたところ、ばねはかりは" +
 			to_string(y) + "gを示した\nこのとき、物体に働く浮力はXニュートンである。\n" +
 			"Xの値を小数点以下第２位を四捨五入して求めよ",answer });
+
+		int p0 = uniform_int_distribution<>(1, 9)(rd);
+		s = uniform_int_distribution<>(5, 10)(rd);
+		h = uniform_int_distribution<>(5, 10)(rd);
+		z = h * p0 + 50;
+		answer = to_string(z / 1000);
+		if (z % 10) {
+			answer += ".";
+			answer += "0" + z % 10;
+		}
+		questions.push_back({
+			"密度" + to_string(p0) + "kg/m^3、底面積" + to_string(s) + "cm^2、高さ" +
+			to_string(h) + "cmの物体を水に沈めようとしたところ、Xcm沈んで静止した。\n" +
+			"Xの値を小数点以下第1位まで求めよ。",answer });
 	}//浮力
 
 	return questions;
