5f994d931cb39ab1144dcf6b2299d67dac138eb4
segawak <segawakeigo@gmail.com>
2025-07-02 15:06:17 +0900

    ばねの問題を追加

--- recruit_quiz/exam_science.cpp
+++ recruit_quiz/exam_science.cpp
@@ -131,5 +131,23 @@ QuestionLIst CreatePhysicsExam()
 			"Xの値を小数点以下第1位まで求めよ。",answer });
 	}//浮力
 
+	{//ばね
+		int a = uniform_int_distribution<>(10, 30)(rd);//自然長(cm)
+		int x = uniform_int_distribution<>(1, a / 2)(rd);//自然長からの変化量(cm)
+		int m = uniform_int_distribution<>(1, 20)(rd);//質量(g)
+		int k = 100 * m / x + 5;//kの100倍を求め、四捨五入のため5を足す
+		string answer = to_string(k / 100);//整数部を文字列に変換
+		k /= 10;//小数点委アk第２位にあたる部分を捨てる
+		if (k % 10) {
+			answer += '.';
+			answer += '0' + k % 10;
+		}
+		questions.push_back({
+			"重力加速度を10m/s^2とする。\n長さ" + to_string(x) + "cmのばねの先端に" +
+			to_string(m) + "gの重りをつけて、天井から吊り下げた。\nすると、ばねの長さが" +
+			to_string(a + x) + "cmになった。\n" +
+			"このばねの「ばね定数」を小数点以下第２位を四捨五入して求めよ。",answer });
+	}//ばね
+
 	return questions;
 }
